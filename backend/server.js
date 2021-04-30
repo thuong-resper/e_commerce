7e4caae6357083c8eebaf3f17091720d086e0df3
thuong-resper <thuongresper@gmail.com>
2021-04-30 20:03:14 +0700

    [main]: prepare for deployment)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
 import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -14,12 +15,11 @@ connectDB();
 
 const app = express();
 
-// accept json data in the body
-app.use(express.json());
-
-if ((process.env.NODE_ENV = "development")) {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(express.json());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -29,24 +29,26 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-app.use(notFound);
-
-app.use(errorHandler);
-
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// Serve static assets if in production
-app.use(express.static(path.join(__dirname, "./amazon-clone/build")));
+// ... other app.use middleware
 
+app.use(express.static(path.join(__dirname, "amazon-clone", "build")));
+
+// Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-  res.sendFile(path.join((__dirname, "./amazon-clone/build/index.html")));
-  AAA;
+  res.sendFile(path.join(__dirname, "amazon-clone", "build", "index.html"));
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
